14ddbd0e12f3f592a34fe5add3af549135f09dc9
Reemabdu213 <11972@holbertonstudents.com>
2025-11-04 18:23:38 +0300

    Update main.h

--- main.h
+++ main.h
@@ -4,9 +4,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/wait.h>
 #include <sys/types.h>
 
 extern char **environ;
+
+char *trim_spaces(char *str);
 
 #endif
