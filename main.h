90488b6d1e4fa7dc53db6d2f98ad2de8ef65b686
Reemabdu213 <11972@holbertonstudents.com>
2025-11-08 16:15:47 +0300

    Update main.h

--- main.h
+++ main.h
@@ -7,11 +7,23 @@
 #include <string.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 
 extern char **environ;
 
+/* String functions */
 char *trim_spaces(char *str);
 int parse_args(char *line, char **args);
-void exec_cmd(char **args, char *line, char *copy);
+
+/* Path functions */
+char *find_in_path(char *cmd);
+
+/* Builtin functions */
+int handle_exit(char **args, char *line, char *copy);
+int handle_env(void);
+int is_builtin(char **args, char *line, char *copy);
+
+/* Execute functions */
+void exec_cmd(char **args, char *line, char *copy, char *shell_name, int cmd_count);
 
 #endif
